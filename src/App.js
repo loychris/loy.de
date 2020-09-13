e95b3a34403ec5eae34b500444465646113b18cb
Chris Loy <cloy202@gmail.com>
2020-09-13 02:44:21 +0200

    added cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Card from './Components/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header>
+         <h1>Christoph Loy</h1>
+       </header>
+       <div>
+         <header>
+           <h2>Most recent books I read</h2>
+         </header>
+         <div className="BookList">
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+         </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import classes from './Card.module.css';
+
+class Card extends Component {
+    render(){
+        return(
+            <article className={classes.card}> 
+                <div className={classes.cardHeader}>
+                  <p>Sep 11th 2020</p>
+                  <h2>Card Tricks are fun!</h2>
+               </div>
+            </article>
+        )
+    }
+}
+
+export default Card;
